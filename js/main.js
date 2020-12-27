43c5020abb85110855d727358aba160b4fdc345e
TroikaNDapp <thecryptosapiens@gmail.com>
2020-12-27 23:18:14 +0100

    Update Js

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@ function UpdateBalance(dAppAddress,Address,StakedToken,GovernToken){
 							$.getJSON('https://nodes-testnet.wavesnodes.com/addresses/data/3N9eE86dXUm7rfc2WWCMLHkaEM4Y8yoNj7u/MaxGovernTokenDepositerKey'),
 							$.getJSON(nodeUrl+'/addresses/data/'+dAppAddress+'?matches='+FundBox+'_'+StakedToken)
 							).done(function (result,PrizeAmount) {
-								console.log("PrizeAmount : ", PrizeAmount[0])						
+								console.log("PrizeAmount : ", PrizeAmount[0][0])						
 								if (result.value == Address+'_'+ GovernToken && PrizeAmount[0].value > 0 )  {
 									// Show Retrieve reward GUI
 									document.getElementById("Rewarding").innerHTML = '<h1>Congratulations ! <br>You push was the highest, and you won the reward prize <h1>'
