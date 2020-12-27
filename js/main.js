c1b0004aa4dddc3efbfac9ad99eb2e2e7bdb291f
TroikaNDapp <thecryptosapiens@gmail.com>
2020-12-27 13:36:11 +0100

    Update Js

--- js/main.js
+++ js/main.js
@@ -95,6 +95,18 @@ function UpdateBalance(dAppAddress,Address,StakedToken,GovernToken){
 			 }								
 				
 		});	
+
+		// Timer for Reward Retrieve
+		$.getJSON("https://nodes-testnet.wavesnodes.com/addresses/data/"+dAppAddress+"_PrizeHeight",  
+		function (FundBoxHeight) {	
+			 if (FundBoxHeight.length == 0) {
+				 document.getElementById("Timer").innerHTML = 'Earned : 0.0 Troika';
+			 }
+			 else{
+				 document.getElementById("Timer").innerHTML = 'Remnaining : '+FundBoxHeight.value;
+			 }								
+				
+		});			
 						     		
 }
 
