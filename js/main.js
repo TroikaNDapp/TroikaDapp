f205cb9b4476c40e31cf17eee20b8ecf46e1864a
TroikaNDapp <thecryptosapiens@gmail.com>
2020-12-27 23:35:22 +0100

    Update Js

--- js/main.js
+++ js/main.js
@@ -116,7 +116,7 @@ function UpdateBalance(dAppAddress,Address,StakedToken,GovernToken){
 								console.log("PrizeAmount : ", PrizeAmount[0][0].value)						
 								console.log(result[0].value)
 								console.log(Address+'_'+ GovernToken)
-								if (result.value == Address+'_'+ GovernToken && PrizeAmount[0][0].value > 0 )  {
+								if (result[0].value == Address+'_'+ GovernToken && PrizeAmount[0][0].value > 0 )  {
 									// Show Retrieve reward GUI
 									document.getElementById("Rewarding").innerHTML = '<h1>Congratulations ! <br>You push was the highest, and you won the reward prize <h1>'
 									document.getElementById("RetrieveReward").innerHTML ='<div class="fund-item" id ="RetrieveReward"><img draggable="false" src="icons/tag.svg" /><h2>Claim reward !</h2>'+						
