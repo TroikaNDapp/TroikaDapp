6e06f2e9f26eb475a337536e453c8903e79512a6
TroikaNDapp <thecryptosapiens@gmail.com>
2020-12-29 22:01:55 +0100

    Update Js

--- js/main.js
+++ js/main.js
@@ -474,16 +474,23 @@ function UnlockMyWallet(){
 }            
 
 function PushReward(){
-	Swal.fire({
-		title: 'How old are you?',
-		icon: 'question',
-		input: 'range',
-		inputLabel: 'Your age',
-		inputAttributes: {
-		  min: 8,
-		  max: 120,
-		  step: 1
-		},
-		inputValue: 25
-	  })
+	swal({
+		title: "An input!",
+		text: "Write something interesting:",
+		type: "input",
+		showCancelButton: true,
+		closeOnConfirm: false,
+		animation: "slide-from-top",
+		inputPlaceholder: "Write something"
+	  },
+	  function(inputValue){
+		if (inputValue === false) return false;
+	  
+		if (inputValue === "") {
+		  swal.showInputError("You need to write something!");
+		  return false
+		}
+	  
+		swal("Nice!", "You wrote: " + inputValue, "success");
+	  });
 };
