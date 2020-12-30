8457969094afdfaf3e2eddd5cf742e0e2fb5c78f
TroikaNDapp <thecryptosapiens@gmail.com>
2020-12-30 20:01:41 +0100

    Update Js

--- js/main.js
+++ js/main.js
@@ -101,7 +101,39 @@ function UpdateBalance(dAppAddress,Address,StakedToken,GovernToken){
 						if (r < 0) {
 							var nextWeek = moment().add(-r, 'days');
 							alert(nextWeek);
-							document.getElementById("Rewarding").innerHTML = '<h1>You still have '+ -r+' Blocks to push <br> with Troika Token and possibly win the rewrad <br>(Around '+-r+' min)</h1>'
+							document.getElementById("Rewarding").innerHTML = '<p id="demo"></p>'+
+							'<script>'+
+							// Set the date we're counting down to
+							'var countDownDate = new Date("'+nextWeek+'").getTime();'+
+
+							// Update the count down every 1 second'+
+							'var x = setInterval(function() {'+
+
+							// Get today's date and time
+							'var now = new Date().getTime();'+
+
+							// Find the distance between now and the count down date'
+							'var distance = countDownDate - now;'+
+
+							// Time calculations for days, hours, minutes and seconds'
+							'var days = Math.floor(distance / (1000 * 60 * 60 * 24));'+
+							'var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));'+
+							'var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));'+
+							'var seconds = Math.floor((distance % (1000 * 60)) / 1000);'+
+
+							'// Display the result in the element with id="demo"'+
+							'document.getElementById("demo").innerHTML = days + "d " + hours + "h "'+
+							'+ minutes + "m " + seconds + "s ";'+
+
+							// If the count down is finished, write some text
+							'if (distance < 0) {'+
+							'	clearInterval(x);'+
+							'	document.getElementById("demo").innerHTML = "EXPIRED";'+
+							'}'+
+							'}, 1000);'+
+							'</script>'
+							
+							//'<h1>You still have '+ -r+' Blocks to push <br> with Troika Token and possibly win the rewrad <br>(Around '+-r+' min)</h1>'
 						}
 						else{
 							// Check if User is winner and withdraw in case he is	
