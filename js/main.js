9127446d77517b127dfcc078409e06a4e1ac3155
TroikaNDapp <thecryptosapiens@gmail.com>
2020-12-31 16:51:21 +0100

    Update Js

--- js/main.js
+++ js/main.js
@@ -112,9 +112,9 @@ function UpdateBalance(dAppAddress,Address,StakedToken,GovernToken){
 							Hours = Math.trunc(-r/60 )
 							Minutes = -r-Math.trunc(-r/60 )
 							if (HighestPushAddress[0].value == Address+"_Push") {
-								document.getElementById("Rewarding").innerHTML = leftPad(Hours, 2)+' <h1>Hours'+ leftPad(Minutes, 2)+' minutes before reward is released</h1> <br> Your Push is actually the highest, if reward is released you would be the winner'
+								document.getElementById("Rewarding").innerHTML = '<h1>'+leftPad(Hours, 2)+' Hours'+ leftPad(Minutes, 2)+' minutes before reward is released</h1> <br> Your Push is actually the highest, if reward is released you would be the winner'
 							}else{
-								document.getElementById("Rewarding").innerHTML = leftPad(Hours, 2)+' <h1>Hours'+ leftPad(Minutes, 2)+'minutes before reward is released</h1> minutes before reward is released '	
+								document.getElementById("Rewarding").innerHTML = '<h1>'+leftPad(Hours, 2)+' Hours'+ leftPad(Minutes, 2)+'minutes before reward is released</h1> minutes before reward is released '	
 							}
 						}
 							
