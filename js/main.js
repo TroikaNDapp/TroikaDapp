37e0922a0325167c870895ef51f6346eec5ca03a
TroikaNDapp <thecryptosapiens@gmail.com>
2021-01-11 19:47:58 +0100

    Update Js

--- js/main.js
+++ js/main.js
@@ -108,6 +108,7 @@ function UpdateBalance(dAppAddress,Address,StakedToken,GovernToken){
 							$.getJSON('https://nodes-testnet.wavesnodes.com/addresses/data/3N9eE86dXUm7rfc2WWCMLHkaEM4Y8yoNj7u/LastPrize')
 							).done(function (UserAPY,LastWinner,LastPrize) {
 								console.log("UserAPY2: ", UserAPY)
+								console.log("User APY now: ", UserAPY[0].value)
 								console.log("LastWinner: ", LastWinner[0].value)
 								console.log("LastPrize: ", LastPrize[0].value/100000000)
 								document.getElementById("ClaimRewardButton").innerHTML ='Your APY : '+UserAPY[0].value+' % <p> Last winner: '+LastWinner[0].value.slice(0,4)+'..'+LastWinner[0].value.slice(31,-5)+'</p><p> Last reward: '+LastPrize[0].value/100000000+'</p>'
