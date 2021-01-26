1e55914d9c184150f0b913d5ae6f254ac17bf11f
TroikaNDapp <thecryptosapiens@gmail.com>
2021-01-26 22:39:02 +0100

    Update Js

--- js/main.js
+++ js/main.js
@@ -173,9 +173,10 @@ function UpdateBalance(dAppAddress,Address,StakedToken,GovernToken){
 									//document.getElementById("Rewarding").innerHTML = '<button class="round dark" onclick="RetrieveReward()" >Retrieve Reward</button>'
 									} else {
 										console.log('NO WINNER')
-										if (HighestPushAddress[0].value  == "" && (PrizeAmount[0].value > 0) )
+										if (HighestPushAddress[0].value  == "" )
 											document.getElementById("Rewarding").innerHTML = '<h1>Prize Unlocked ! <p>Waiting for Users to Push, be the first to push for the reward  </p> <h1>'
-										if (HighestPushAddress[0].value  != "" && (PrizeAmount[0].value == 0) )
+																					
+										if (HighestPushAddress[0].value  != "" && (PrizeAmount[0].value > 0) )
 											document.getElementById("Rewarding").innerHTML = '<h1>Prize awarded ! <h1><p><h2> User push  '+HighestPushAddress[0].value.slice(0,4)+'..'+HighestPushAddress[0].value.slice(-4)+
 											                                                 ' was the highest </p><p> Winner has One day to withdraw Reward, After that and if Reward is not withdrawn, Users can Push again for the same reward</p> <h2>'
 										$.when(							      	      											
