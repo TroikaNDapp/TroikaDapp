b0f4d35fcf3a5658c1cde74e2da04cc09f2ff4c9
TroikaNDapp <thecryptosapiens@gmail.com>
2021-02-25 21:56:04 +0100

    Update Js

--- js/main.js
+++ js/main.js
@@ -181,8 +181,8 @@ function UpdateBalance(dAppAddress,Address,StakedToken,GovernToken){
 											console.log('HeightBlockch: ',HeightBlockch[0].height)
 											console.log('Delayblock: ', Delayblock[0].value)
 											Days =  Math.trunc((HeightBlockch[0].height-PrizeHeight[0].value-Delayblock[0].value)/1440)
-											Hours = ((HeightBlockch[0].height-PrizeHeight[0].value-Delayblock[0].value)-Days*1440)/60
-											Min = (HeightBlockch[0].height-PrizeHeight[0].value-Delayblock[0].value)-Hours*60-Days*1440
+											Hours = Math.trunc(((HeightBlockch[0].height-PrizeHeight[0].value-Delayblock[0].value)-Days*1440)/60)
+											Min =  Math.trunc((HeightBlockch[0].height-PrizeHeight[0].value-Delayblock[0].value)-Hours*60-Days*1440)
 											document.getElementById("Rewarding").innerHTML = '<h1>Prize awarded ! <h1><p><h2> User push  '+HighestPushAddress[0].value.slice(0,4)+'..'+HighestPushAddress[0].value.slice(-4)+
 											                                                 ' was the highest </p><p> Winner has '+Days+' Day '+Hours+' H ' +Min+' min to withdraw Reward, After that and if Reward is not withdrawn, Users can Push again for the same reward</p> <h2>'
 										$.when(							      	      											
