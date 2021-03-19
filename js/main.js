6f1f6db2292a83ecfaa3a0ca7bd719e4a285b62b
TroikaNDapp <thecryptosapiens@gmail.com>
2021-03-19 07:26:27 +0100

    Update Js

--- js/main.js
+++ js/main.js
@@ -204,9 +204,9 @@ function UpdateBalance(dAppAddress,Address,StakedToken,GovernToken){
 											RetrieveCountDown = (PrizeHeight[0].value+Delayblock[0].value-HeightBlockch[0].height)
 											if (RetrieveCountDown < 0){
 												console.log("Countdown: ", RetrieveCountDown)
-												Days  =  Math.trunc(RetrieveCountDown/1440)
-												Hours =  Math.trunc((RetrieveCountDown-Days*1440)/60)
-												Min   =  Math.trunc(RetrieveCountDown-Hours*60-Days*1440)
+												Days  =  Math.trunc(-RetrieveCountDown/1440)
+												Hours =  Math.trunc((-RetrieveCountDown-Days*1440)/60)
+												Min   =  Math.trunc(-RetrieveCountDown-Hours*60-Days*1440)
 												document.getElementById("Rewarding").innerHTML = '<h1>Prize awarded ! <h1><p><h2> User push  '+HighestPushAddress[0].value.slice(0,4)+'..'+HighestPushAddress[0].value.slice(-4)+
 											                                                 ' was the highest </p><p> Winner has '+Days+' Day '+Hours+' H ' +Min+' min to withdraw Reward, After that and if Reward is not withdrawn, Users can Push again for the same reward</p> <h2>'
 											}
